d1f7ebc7e57523fe44a749cdcd13f5d08c514da4
asantesylvesterkwame <intelligentson17@gmail.com>
2023-08-16 00:50:35 +0000

    Created a route for Cv Engine

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { BrowserRouter, Router, Routes, Route } from "react-router-dom";
+import CvEngine from "./pages/CvEngine";
 
 function App() {
-
-
   return (
     <>
-      
+      <Routes>
+        <Route
+          path="/cv-engine"
+          element={<CvEngine />}
+        />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
