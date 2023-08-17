ae12eb3fa681271f502a327b890d6352f700bcf3
asantesylvesterkwame <intelligentson17@gmail.com>
2023-08-17 02:15:42 +0000

    created and added elements to my FormInput element component

---
+++ src/Components/FormInput.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+const FormInput = ({
+  Label,
+  type,
+  width,
+  height,
+  placeholder,
+  recommendation,
+}) => {
+  const inputStyle = {
+    width: width + "em",
+    height: height + "em",
+  };
+  return (
+    <div>
+      <form
+        action=""
+        className="flex flex-col py-2.5 gap-2"
+      >
+        <label
+          htmlFor=""
+          className="font-semibold flex items-center gap-2"
+        >
+          {Label}
+          <span className="text-sm text-gray-400">{recommendation}</span>
+        </label>
+        <input
+          type={type}
+          placeholder={placeholder}
+          className="border rounded-xl py-1 px-3   outline-blue-700 outline-neon transition-all "
+          style={inputStyle}
+        />
+      </form>
+    </div>
+  );
+};
+
+export default FormInput;
