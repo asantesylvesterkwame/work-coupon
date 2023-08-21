00977b35ef6dcd14ed57117f7fddacc54b6b6632
asantesylvesterkwame <intelligentson17@gmail.com>
2023-08-21 06:24:21 +0000

    Added a classname prop to the form Input

--- src/Components/FormInput.jsx
+++ src/Components/FormInput.jsx
@@ -7,21 +7,22 @@ const FormInput = ({
   height,
   placeholder,
   recommendation,
-  onChange
+  onChange,
+  className
 }) => {
   const inputStyle = {
     width: width + "em",
     height: height + "em",
   };
   return (
-    <div>
+    <div className={className}>
       <form
         action=""
         className="flex flex-col py-2.5 gap-2"
       >
         <label
           htmlFor=""
-          className="font-semibold flex items-center gap-2"
+          className="font-semibold flex items-center gap-2 text-sm"
         >
           {Label}
           <span className="text-sm text-gray-400">{recommendation}</span>
@@ -29,7 +30,7 @@ const FormInput = ({
         <input
           type={type}
           placeholder={placeholder}
-          className="border rounded-xl py-1 px-3   outline-blue-700 outline-neon transition-all "
+          className="border rounded-xl py-1 px-3 bg-[#f3f4f6] text-sm outline-blue-700 outline-neon transition-all "
           style={inputStyle}
           onChange={onChange}
         />
