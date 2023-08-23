7b93faf09fb6b2d93d3a7c87c26746e5ba170047
asantesylvesterkwame <intelligentson17@gmail.com>
2023-08-23 11:14:41 +0000

    added an onClick prop

--- src/Components/SecondaryButton.jsx
+++ src/Components/SecondaryButton.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const SecondaryButton = ({text}) => {
+const SecondaryButton = ({text, onClick}) => {
   return (
     <div>
-      <button className='border p-2 text-sm rounded-md text-gray-500 border-gray-500 '>{text}</button>
-    </div>
+      <button onClick={onClick} className='border p-2 text-sm rounded-md text-gray-500 border-gray-500 hover:bg-gray-500 hover:text-white transition active:text-black'>{text}</button>
+    </div> 
   );
 }
 
