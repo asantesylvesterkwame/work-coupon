fd4f86e1644e4b5a8fbdca0bbf24e23cd2411932
asantesylvesterkwame <intelligentson17@gmail.com>
2023-08-23 11:15:15 +0000

    added an OnClick Prop

--- src/Components/SubmitBtn.jsx
+++ src/Components/SubmitBtn.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
-const SubmitBtn = ({text}) => {
+const SubmitBtn = ({text, onClick}) => {
   return (
     <div>
-      <button className="border p-2 text-sm rounded-md text-white bg-blue-600 ">
+      <button
+        onClick={onClick}
+        className="hover:bg-blue-800 hover:text-white transition active:text-black border p-2 text-sm rounded-md text-white bg-blue-600 "
+      >
         {text}
       </button>
     </div>
