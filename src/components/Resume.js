521d45dac767cab29c388b07060e13f403ef8df3
asantesylvesterkwame <intelligentson17@gmail.com>
2023-08-30 20:44:26 +0000

    added the download resue functionality

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -3,11 +3,27 @@ import PersonalInfoSection from "./personal-info/PersonalInfoSection";
 import EducationInfoSection from "./education/EducationInfoSection";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import ExperienceInfoSection from "./experience/ExperienceInfoSection";
+import html2canvas from "html2canvas";
+import jsPDF from "jspdf";
 
 function Resume({ personalInfo, sections, layout }) {
+  const handleDownload = () => {
+    const input = document.getElementById("resume");
+
+    html2canvas(input).then((canvas) => {
+      const imgData = canvas.toDataURL("image/png");
+      const pdf = new jsPDF();
+      pdf.addImage(imgData, "JPEG", 0, 0);
+      pdf.save("download.pdf");
+    });
+  };
+
   return (
     <div className="resume-container">
-      <div className={`resume ${layout}`}>
+      <div
+        className={`resume ${layout}`}
+        id="resume"
+      >
         <PersonalInfoSection
           fullName={personalInfo.fullName}
           email={personalInfo.email}
@@ -18,6 +34,14 @@ function Resume({ personalInfo, sections, layout }) {
           <EducationInfoSection educations={sections.educations} />
           <ExperienceInfoSection experiences={sections.experiences} />
         </div>
+      </div>
+      <div className="download-button">
+        <button
+          className="download"
+          onClick={handleDownload}
+        >
+          Download
+        </button>
       </div>
     </div>
   );
